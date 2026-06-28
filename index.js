3ebd354d14fb83de6a27b0e2ea20fe65259c2c83
sada240 <sada240@gmail.com>
2026-06-28 11:26:57 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 "use strict";
 function sayHello(name) {
-    return `こんにちは、${name}さん！Web画面に変更れました！`;
+    return `こんにちは、${name}さん！Web画面に変更されました！`; // 💡ここを修正
 }
 const userName = "sada240";
-// --- ここからWeb画面に表示するためのコード ---
-// 1. HTMLの中から id="display" という場所を探す
 const displayArea = document.getElementById('display');
-// 2. もし見つかったら、その中身を書き換える
 if (displayArea) {
     displayArea.innerText = sayHello(userName);
-}
+} // 💡最後の閉じカッコがしっかりあるか確認！
